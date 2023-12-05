6054439c86b0200714deb575e3c56c28382933fc
sakshic75 <sakshi75.sc@gmail.com>
2023-12-05 20:04:16 +1100

    Add/Modify UI for Spec Ops monitoring page

--- src/pages/security-ops-monitoring.tsx
+++ src/pages/security-ops-monitoring.tsx
@@ -1,35 +1,38 @@
 import VoltageInformation from 'n/components/VoltageDataComponents/VoltageInfoComponent';
 import React from 'react';
-import {BatteryAutomotive, ChartCandle, Activity} from 'tabler-icons-react';
+import { BatteryAutomotive, ChartCandle, Activity } from 'tabler-icons-react';
 import InverterCommandCenter from '../components/CommandCenterComponents/CommandCenter';
 import InformationAccordion from '../components/SetupComponents/DerTechnicalSpecifications';
 import DerCompliance from 'n/components/SetupComponents/DerCompliance';
+import NetworkMonitoringLogs from 'n/components/SetupComponents/NetworkMonitoringLogs';
 
 export default function Settings() {
   return (
     <div className="page-layout">
       <div className="top">
         <div className="left">
-          <div className="left-heading"> 
-            <BatteryAutomotive size="3rem" color='green'/>
+          <div className="left-heading">
+            <BatteryAutomotive size="3rem" color='green' />
             <h3>DER Technical Specifications</h3>
           </div>
           <InformationAccordion />
         </div>
         <div className="right">
-        <div className="right-heading"> 
-            <Activity size="3rem" color='green'/>
+          <div className="right-heading">
+            <Activity size="3rem" color='green' />
             <h3>Compliance Specifications</h3>
           </div>
-          <DerCompliance/>
+          <DerCompliance />
         </div>
       </div>
       <div className="bottom">
-        <div className="bottom-heading"> 
-          <ChartCandle size="3rem" color='green'/>
+        <div className="bottom-heading">
+          <ChartCandle size="3rem" color='green' />
           <h2>Network Monitoring and logs</h2>
         </div>
-      
+        <div className='bottom-b'>
+          <NetworkMonitoringLogs />
+        </div>
       </div>
       <style jsx>{`
         .page-layout {
