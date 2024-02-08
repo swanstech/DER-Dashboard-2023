120a5bd6c6ec9458747d0ed812481c0dc479f80b
sakshic75 <sakshi75.sc@gmail.com>
2024-02-09 10:38:09 +1100

    Removed BaseApp shell

--- src/components/BaseAppShell.tsx
+++ src/components/BaseAppShell.tsx
@@ -1,28 +1,31 @@
-import { AppShell, useMantineTheme } from "@mantine/core";
-import HeaderComponent from "../components/Header";
-import NavbarComponent from "../components/Navbar";
+// import { AppShell, useMantineTheme } from "@mantine/core";
+// import HeaderComponent from "../components/Header";
+// import NavbarComponent from "../components/Navbar";
 
-function BaseAppShell({ children }: any) {
-  const theme = useMantineTheme();
+// function BaseAppShell({ children }: any) {
+//   const theme = useMantineTheme();
 
-  return (
-    <AppShell
-      styles={{
-        main: {
-          background:
-            theme.colorScheme === "dark"
-              ? theme.colors.dark[8]
-              : theme.colors.gray[0],
-        },
-      }}
-      navbarOffsetBreakpoint="sm"
-      asideOffsetBreakpoint="sm"
-      navbar={<NavbarComponent />}
-      header={<HeaderComponent />}
-    >
-      {children}
-    </AppShell>
-  );
-}
+//   return (
+//     <AppShell
+//       styles={{
+//         main: {
+//           background:
+//             theme.colorScheme === "dark"
+//               ? theme.colors.dark[8]
+//               : theme.colors.gray[0],
+//         },
+//       }}
+//       navbarOffsetBreakpoint="sm"
+//       asideOffsetBreakpoint="sm"
+//       navbar={<NavbarComponent />}
+//       header={<HeaderComponent userRoles={[]} userProfile={{
+//         fullName: "",
+//         email: ""
+//       }} keycloakInstance={new Keycloak} />}
+//     >
+//       {children}
+//     </AppShell>
+//   );
+// }
 
-export default BaseAppShell;
+// export default BaseAppShell;
