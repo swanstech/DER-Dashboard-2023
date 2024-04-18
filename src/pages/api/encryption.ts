1d4769e8f2d1f83dda6756ff565ee444a0cd09d8
sakshic75 <sakshi75.sc@gmail.com>
2024-04-18 14:05:15 +1000

    added

--- src/pages/api/encryption.ts
+++ src/pages/api/encryption.ts
@@ -17,14 +17,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
       // Encode the JSON data to base64
 
-      let encodedData = data.replace(/'/g, '"');
-      encodedData = encodedData.replace(/\n/g, '');
+      
+      let encodedData = data.replace(/\n/g, '');
       //encodedData = JSON.stringify(encodedData);
       console.log("data ", data);
       console.log("encoded data ", encodedData);
 
       // Execute the Python script with the provided data as command-line arguments
-      exec(`python3 ${absolutePathToPythonScript} '${encodedData}'`, (error, stdout, stderr) => {
+      exec(`python3 ${absolutePathToPythonScript} "${encodedData}"`, (error, stdout, stderr) => {
       //exec(`python3 /usr/src/app/python_files/encryption.py '{"CreatedOn": "2024-04-16 03:26:36", "DERActivePower": "537.0", "DerApparentPower": "537.0009155273438", "DerCurrent": "2.3499999046325684", "DerFrequency": "51.98999786376953", "DerPowerFactor": "-99.99983215332031", "DerReactivePower": "1.0", "DerVoltage": "180.5"}'`, (error, stdout, stderr) => {
         if (error) {
           console.error(`Error executing encryption script: ${error}`);
